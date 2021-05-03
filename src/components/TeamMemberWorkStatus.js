3446d83833f0d42d537ace97e3d941397f7e60ef
Kshitija-Patange <kshitijapatangebharat@gmail.com>
2021-05-03 10:30:37 +0530

    first commit

---
+++ src/components/TeamMemberWorkStatus.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import React from 'react'
+import { ProgressBar } from 'react-bootstrap'
+
+const axios = require('axios').default;
+
+class TeamMemberWorkStatus extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      teamMembers: null,
+      input: 5
+    };
+  }
+
+
+  componentDidMount() {
+    axios.put('http://localhost:8080/api/kanban_board/viewWorkStatus/1').then((response) => {
+      this.setState({ teamMembers: response.data.teamMemberEntity })
+      console.log(response.data, "Hi");
+    });
+  }
+
+  render() {
+     
+    return (
+      <div>
+        <div style={{backgroundColor: '#252525',  padding: '1em'}} >
+          <h2>My Work Status</h2>
+          <p>your work status dependends on your tasks</p>
+          <ProgressBar  style={{height: '1em'}} now={this.state.teamMembers?this.state.teamMembers.workStatus:null}/>
+          <div>{this.state.teamMembers?this.state.teamMembers.workStatus:null}%</div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default TeamMemberWorkStatus
